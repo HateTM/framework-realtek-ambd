bd40b8aabafbac2f6092a13676416eef07ee5b80
Sergey Shlukov <ichizakurain@gmail.com>
2026-04-18 05:00:00 +0300

    Delete component directory

--- component/common/example/audio_track/example_audio_track.h
+++
@@ -1,8 +0,0 @@
-#ifndef _EXAMPLE_AUDIO_TRACK_H_
-#define _EXAMPLE_AUDIO_TRACK_H_
-
-#include "platform_stdlib.h"
-
-u32 example_track_test(u16 argc, u8 *argv[]);
-
-#endif
--- component/common/example/player/example_player.h
+++
@@ -1,6 +0,0 @@
-#ifdef _EXAMPLE_PLAYER_H_
-#define _EXAMPLE_PLAYER_H_
-
-void example_player(u16 argc, u8 *argv[]);
-
-#endif
